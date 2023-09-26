5189be9b7af4d5b48256353a7c1890b212c02ac6
GabrielGharibyan <gabriel.gharibyan@mail.ru>
2023-09-26 11:12:36 +0400

    bag fix

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -33,34 +33,7 @@ $this->registerLinkTag(['rel' => 'icon', 'type' => 'image/x-icon', 'href' => Yii
 <?php
 echo $this->render('dashboard');
 ?>
-<!--<header id="header">-->
-<!--    --><?php
-//    NavBar::begin([
-//        'brandLabel' => Yii::$app->name,
-//        'brandUrl' => Yii::$app->homeUrl,
-//        'options' => ['class' => 'navbar-expand-md navbar-dark bg-dark fixed-top']
-//    ]);
-//    echo Nav::widget([
-//        'options' => ['class' => 'navbar-nav'],
-//        'items' => [
-//            ['label' => 'Home', 'url' => ['/site/index']],
-//            ['label' => 'About', 'url' => ['/site/about']],
-//            ['label' => 'Contact', 'url' => ['/site/contact']],
-//            Yii::$app->user->isGuest
-//                ? ['label' => 'Login', 'url' => ['/site/login']]
-//                : '<li class="nav-item">'
-//                    . Html::beginForm(['/site/logout'])
-//                    . Html::submitButton(
-//                        'Logout (' . Yii::$app->user->identity->username . ')',
-//                        ['class' => 'nav-link btn btn-link logout']
-//                    )
-//                    . Html::endForm()
-//                    . '</li>'
-//        ]
-//    ]);
-//    NavBar::end();
-//    ?>
-<!--</header>-->
+
 
 <main id="main" class="flex-shrink-0" role="main">
     <div class="container">
@@ -72,14 +45,6 @@ echo $this->render('dashboard');
     </div>
 </main>
 
-<!--<footer id="footer" class="mt-auto py-3 bg-light">-->
-<!--    <div class="container">-->
-<!--        <div class="row text-muted">-->
-<!--            <div class="col-md-6 text-center text-md-start">&copy; My Company --><?php //= date('Y') ?><!--</div>-->
-<!--            <div class="col-md-6 text-center text-md-end">--><?php //= Yii::powered() ?><!--</div>-->
-<!--        </div>-->
-<!--    </div>-->
-<!--</footer>-->
 
 <?php $this->endBody() ?>
 </body>
