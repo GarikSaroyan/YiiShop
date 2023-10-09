7954df3a4cde95f86af6be676319480279d2053a
GarikSaroyan <gariksaroyan00@mail.ru>
2023-10-09 15:18:01 +0400

    bug fixing

--- views/layouts/dashboard.php
+++ views/layouts/dashboard.php
@@ -7,17 +7,18 @@ $this->registerCssFile("@web/css/dashboard.css", [], 'css-print-theme');
 ?>
 
 <div class="sidebar">
-    <h1 class="logo">SoftTech</h1>
+    <div class="sidebarFixed">
+        <h1 class="logo">SoftTech</h1>
 
-    <ul class="nav">
-        <li><a href="/orders"><i class="fa fa-windows"></i>Orders</a></li>
-<!--        <li ><a href="/about"><i class="fa fa-shopping-bag"></i>Order Item</a></li>-->
-        <li><a href="/product"><i class="fa fa-pie-chart"></i>Product</a></li>
-        <li><a href="/store"><i class="fa fa-cube"></i>Store</a></li>
-        <li ><a href="/category"><i class="fa fa-database"></i>Category</a></li>
-        <li><a href="/target"><i class="fa fa-tag"></i>Target</a></li>
+        <ul class="nav">
+            <li><a href="/orders"><i class="fa fa-windows"></i>Orders</a></li>
+            <!--        <li ><a href="/about"><i class="fa fa-shopping-bag"></i>Order Item</a></li>-->
+            <li><a href="/product"><i class="fa fa-pie-chart"></i>Product</a></li>
+            <li><a href="/store"><i class="fa fa-cube"></i>Store</a></li>
+            <li><a href="/category"><i class="fa fa-database"></i>Category</a></li>
+            <li><a href="/target"><i class="fa fa-tag"></i>Target</a></li>
 
-    </ul>
-
+        </ul>
+    </div>
 
 </div>
